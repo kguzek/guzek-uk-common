0ea0ebc50ba298692b326bb02f6d017f2510663e
Konrad (PC) <konrad@guzek.uk>
2024-12-17 17:06:53 +0100

    relocate .env to project root

--- src/setup.ts
+++ src/setup.ts
@@ -15,7 +15,7 @@ export function setupEnvironment() {
   // In production mode, this code is run from /api/v1/dist/index.js
   // In development mode, it is in /api/v1/index.ts
   // The env file is in /api/.env, so adjust accordingly
-  const ENV_FILE_PATH = DEBUG_MODE ? "../.env" : "../../.env";
+  const ENV_FILE_PATH = DEBUG_MODE ? ".env" : "../.env";
   const dotEnvPath = resolve(appDirectory, ENV_FILE_PATH);
   dotenv.config({ path: dotEnvPath });
 
