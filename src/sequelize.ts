54305b52901eb4dbd292dc2aca93a0c0643e0aa0
Konrad (PC) <konrad@guzek.uk>
2024-12-18 20:02:07 +0100

    migrate to sequelize-typescript

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,25 +1,17 @@
-import { DataTypes, Model, Sequelize } from "sequelize";
+import {
+  Sequelize,
+  Model,
+  Table,
+  DataType,
+  Column,
+  BelongsTo,
+  HasOne,
+  HasMany,
+} from "sequelize-typescript";
+import { getLogger } from "./logger";
+import { LatLngArr, WatchedShowData } from "./models";
 
-const SESSION_INFO = {
-  DB_HOST: process.env.MY_SQL_DB_HOST ?? "127.0.0.1",
-  DB_PORT: process.env.MY_SQL_DB_PORT ?? "3306",
-  DB_USER: process.env.MY_SQL_DB_USER ?? "root",
-  DB_PASSWORD: process.env.MY_SQL_DB_PASSWORD ?? "",
-  DB_NAME: process.env.MY_SQL_DB_NAME ?? "database",
-  DB_CONNECTION_LIMIT: parseInt(process.env.MY_SQL_DB_CONNECTION_LIMIT ?? "4"),
-};
-
-// Initialise Sequelize
-const sequelize = new Sequelize(
-  SESSION_INFO.DB_NAME,
-  SESSION_INFO.DB_USER,
-  SESSION_INFO.DB_PASSWORD,
-  {
-    host: SESSION_INFO.DB_HOST,
-    dialect: "mysql",
-    logging: process.env.NODE_ENV === "development" && console.log,
-  }
-);
+const logger = getLogger(__filename);
 
 const ensureJson = (dataValue: any) =>
   typeof dataValue === "string" ? JSON.parse(dataValue) : dataValue;
@@ -28,293 +20,199 @@ const ensureJson = (dataValue: any) =>
 export const sanitiseShowName = (showName: string) =>
   showName.replace(/[.+]/g, " ").replace(/:\//g, "").trim();
 
-export class Page extends Model {}
-export class PageContent extends Model {}
-export class User extends Model {}
-export class Token extends Model {}
-export class Updated extends Model {}
-export class TuLalem extends Model {}
-export class UserShows extends Model {}
-export class WatchedEpisodes extends Model {}
-export class DownloadedEpisode extends Model {}
+@Table({})
+export class Page extends Model {
+  @Column({ primaryKey: true, autoIncrement: true })
+  id!: number;
+  @Column({ allowNull: false, field: "title_en" })
+  titleEN!: string;
+  @Column({ allowNull: false, field: "title_pl" })
+  titlePL!: string;
+  @Column({ allowNull: false })
+  url!: string;
+  @Column({ field: "local_url", allowNull: false })
+  localUrl!: boolean;
+  @Column({ field: "admin_only", allowNull: false })
+  adminOnly!: boolean;
+  @Column({ field: "should_fetch", allowNull: false })
+  shouldFetch!: boolean;
+}
 
-Page.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    titleEN: {
-      type: DataTypes.STRING,
-      field: "title_en",
-      allowNull: false,
-    },
-    titlePL: {
-      type: DataTypes.STRING,
-      field: "title_pl",
-      allowNull: false,
-    },
-    url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    localUrl: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      field: "local_url",
-    },
-    adminOnly: {
-      field: "admin_only",
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-    shouldFetch: {
-      field: "should_fetch",
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    modelName: "Page",
-    timestamps: false,
-    tableName: "pages",
-  }
-);
+@Table({})
+export class PageContent extends Model {
+  @Column({ allowNull: false, field: "content_en" })
+  contentEN!: string;
+  @Column({ allowNull: false, field: "content_pl" })
+  contentPL!: string;
 
-PageContent.init(
-  {
-    contentEN: {
-      type: DataTypes.TEXT,
-      field: "content_en",
-    },
-    contentPL: {
-      type: DataTypes.TEXT,
-      field: "content_pl",
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    modelName: "PageContent",
-    tableName: "page_content",
-  }
-);
+  @BelongsTo(() => Page, { foreignKey: "page_id" })
+  page!: Page;
+}
 
-User.init(
-  {
-    uuid: {
-      type: DataTypes.STRING(36),
-      allowNull: false,
-      unique: true,
-      primaryKey: true,
-    },
-    username: {
-      type: DataTypes.STRING(64),
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    hash: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    salt: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    admin: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  },
-  {
-    sequelize,
-    modelName: "User",
-    tableName: "users",
-    // Rename properties to match database columns
-    createdAt: "created_at",
-    updatedAt: "modified_at",
-  }
-);
-
-Token.init(
-  {
-    value: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      primaryKey: true,
-    },
-  },
-  {
-    sequelize,
-    modelName: "Token",
-    tableName: "tokens",
-    // Rename properties to match database columns
-    createdAt: "created_at",
-    updatedAt: false,
-  }
-);
-
-Updated.init(
-  {
-    endpoint: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    timestamp: {
-      type: DataTypes.BIGINT.UNSIGNED,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    modelName: "Updated",
-    tableName: "updated",
-  }
-);
-
-TuLalem.init(
-  {
-    coordinates: {
-      type: DataTypes.GEOMETRY("POINT"),
-      allowNull: false,
-      get() {
-        const coords = this.getDataValue("coordinates").coordinates;
-        const [lng, lat] = coords;
-        return [lat, lng];
-      },
-    },
-  },
-  {
-    sequelize,
-    modelName: "TuLalem",
-    tableName: "tu_lalem",
-    createdAt: "timestamp",
-    updatedAt: false,
-  }
-);
-
-UserShows.init(
-  {
-    userUUID: {
-      type: DataTypes.STRING(36),
-      allowNull: false,
-      unique: true,
-      field: "user_uuid",
-      primaryKey: true,
-      references: { model: User, key: "uuid" },
-    },
-    likedShows: {
-      type: DataTypes.JSON(),
-      allowNull: false,
-      field: "liked_shows",
-      get() {
-        return ensureJson(this.getDataValue("likedShows"));
-      },
-    },
-    subscribedShows: {
-      type: DataTypes.JSON(),
-      allowNull: false,
-      field: "subscribed_shows",
-      get() {
-        return ensureJson(this.getDataValue("subscribedShows"));
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    modelName: "UserShows",
-    tableName: "user_shows",
-  }
-);
-
-WatchedEpisodes.init(
-  {
-    userUUID: {
-      type: DataTypes.STRING(36),
-      allowNull: false,
-      unique: true,
-      field: "user_uuid",
-      primaryKey: true,
-      references: { model: User, key: "uuid" },
-    },
-    watchedEpisodes: {
-      type: DataTypes.JSON(),
-      allowNull: false,
-      field: "watched_episodes",
-      get() {
-        return ensureJson(this.getDataValue("watchedEpisodes"));
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    modelName: "WatchedEpisodes",
-    tableName: "watched_episodes",
-  }
-);
-
-DownloadedEpisode.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    showId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: "show_id",
-    },
-    showName: {
-      type: DataTypes.STRING(32),
-      allowNull: false,
-      set(value: string) {
-        const sanitised = sanitiseShowName(value);
-        this.setDataValue("showName", sanitised);
-      },
-      field: "show_name",
-    },
-    season: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    episode: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    modelName: "DownloadedEpisode",
-    tableName: "downloaded_episodes",
-  }
-);
-
-PageContent.belongsTo(Page, {
-  foreignKey: {
-    name: "pageID",
+@Table({ timestamps: true, updatedAt: "modified_at" })
+export class User extends Model {
+  @Column({
+    type: DataType.STRING(36),
     allowNull: false,
-    field: "page_id",
-  },
-});
+    unique: true,
+    primaryKey: true,
+  })
+  uuid!: string;
+  @Column({ type: DataType.STRING(64), allowNull: false })
+  username!: string;
+  @Column({ allowNull: false })
+  email!: string;
+  @Column({ allowNull: false })
+  hash!: string;
+  @Column({ allowNull: false })
+  salt!: string;
+  @Column({ defaultValue: false })
+  admin!: boolean;
+  @HasOne(() => UserShows, { foreignKey: "user_uuid" })
+  userShows!: UserShows | null;
+  @HasOne(() => WatchedEpisodes, { foreignKey: "user_uuid" })
+  watchedEpisodes!: WatchedEpisodes | null;
+  @HasMany(() => TuLalem, { foreignKey: "user_uuid" })
+  tuLalem!: TuLalem[];
+}
 
-User.hasMany(TuLalem, {
-  foreignKey: {
-    name: "userUUID",
+@Table({ timestamps: true, updatedAt: false })
+export class Token extends Model {
+  @Column({ allowNull: false, primaryKey: true })
+  value!: string;
+}
+
+@Table({})
+export class Updated extends Model {
+  @Column({ allowNull: false })
+  endpoint!: string;
+  @Column({ allowNull: false })
+  timestamp!: number;
+}
+
+@Table({ timestamps: true, createdAt: "timestamp", updatedAt: false })
+export class TuLalem extends Model {
+  @Column({
     allowNull: false,
-    field: "user_uuid",
-  },
-});
+    type: DataType.GEOMETRY("POINT"),
+    get() {
+      const coords = this.getDataValue("coordinates").coordinates;
+      const [lng, lat] = coords;
+      return [lat, lng];
+    },
+  })
+  coordinates!: LatLngArr;
+  @BelongsTo(() => User, { foreignKey: "user_uuid" })
+  user!: User;
+}
 
-User.hasOne(UserShows, { foreignKey: "userUUID" });
+@Table({})
+export class UserShows extends Model {
+  @Column({ allowNull: false, field: "user_uuid", primaryKey: true })
+  userUUID!: string;
+  @Column({
+    allowNull: false,
+    field: "liked_shows",
+    type: DataType.JSON,
+    get() {
+      return ensureJson(this.getDataValue("liked_shows"));
+    },
+  })
+  likedShows!: number[];
+  @Column({
+    allowNull: false,
+    field: "subscribed_shows",
+    type: DataType.JSON,
+    get() {
+      return ensureJson(this.getDataValue("subscribed_shows"));
+    },
+  })
+  subscribedShows!: number[];
+  @BelongsTo(() => User, { foreignKey: "user_uuid" })
+  user!: User;
+}
 
-User.hasOne(WatchedEpisodes, { foreignKey: "userUUID" });
+@Table({})
+export class WatchedEpisodes extends Model {
+  @Column({ allowNull: false, field: "user_uuid", primaryKey: true })
+  userUUID!: string;
+  @Column({
+    allowNull: false,
+    field: "watched_episodes",
+    type: DataType.JSON,
+    get() {
+      return ensureJson(this.getDataValue("watched_episodes"));
+    },
+  })
+  watchedEpisodes!: WatchedShowData;
+  @BelongsTo(() => User, { foreignKey: "user_uuid" })
+  user!: User;
+}
+
+@Table({})
+export class DownloadedEpisode extends Model {
+  @Column({ primaryKey: true, autoIncrement: true })
+  id!: number;
+  @Column({ allowNull: false, field: "show_id" })
+  showId!: number;
+  @Column({
+    allowNull: false,
+    field: "show_name",
+    set(value: string) {
+      this.setDataValue("show_name", sanitiseShowName(value));
+    },
+  })
+  showName!: string;
+  @Column({ allowNull: false })
+  season!: number;
+  @Column({ allowNull: false })
+  episode!: number;
+}
+
+export function initialiseSequelize(debugMode: boolean) {
+  const session = {
+    host: process.env.MY_SQL_DB_HOST ?? "127.0.0.1",
+    port: process.env.MY_SQL_DB_PORT ?? "3306",
+    user: process.env.MY_SQL_DB_USER ?? "root",
+    password: process.env.MY_SQL_DB_PASSWORD,
+    database: process.env.MY_SQL_DB_NAME ?? "database",
+    // connectionLimit: parseInt(process.env.MY_SQL_DB_CONNECTION_LIMIT ?? "4"),
+  };
+
+  const sequelize = new Sequelize(
+    session.database,
+    session.user,
+    session.password,
+    {
+      host: session.host,
+      dialect: "mysql",
+      logging: debugMode && console.log,
+      models: [
+        Page,
+        PageContent,
+        User,
+        Token,
+        Updated,
+        TuLalem,
+        UserShows,
+        WatchedEpisodes,
+        DownloadedEpisode,
+      ],
+      define: {
+        underscored: true,
+        timestamps: false,
+      },
+    }
+  );
+
+  // Establish the database connection
+  sequelize
+    .authenticate()
+    .then(() => logger.debug("Database connection established"))
+    .catch((error) => logger.error("Error connecting to the database:", error));
+
+  // Sync models (if needed)
+  if (debugMode) {
+    sequelize.sync();
+  }
+}
--- src/setup.ts
+++ src/setup.ts
@@ -1,5 +1,6 @@
 import { dirname, resolve } from "path";
 import dotenv from "dotenv";
+import { initialiseSequelize } from "./sequelize";
 
 /**
  * Reads the `.env` file and prepares for server start.
@@ -11,18 +12,19 @@ export function setupEnvironment() {
     throw new Error("Unable to determine the entry point of the application.");
   }
   const appDirectory = dirname(entryPoint);
-  const DEBUG_MODE = process.env.NODE_ENV === "development";
+  const debugMode = process.env.NODE_ENV === "development";
   // In production mode, this code is run from /api/v1/dist/index.js
   // In development mode, it is in /api/v1/index.ts
   // The env file is in /api/.env, so adjust accordingly
-  const ENV_FILE_PATH = DEBUG_MODE ? ".env" : "../.env";
+  const ENV_FILE_PATH = debugMode ? ".env" : "../.env";
   const dotEnvPath = resolve(appDirectory, ENV_FILE_PATH);
   dotenv.config({ path: dotEnvPath });
 
-  if (DEBUG_MODE) {
+  if (debugMode) {
     // Add newline before app output for readability
     console.log();
   }
 
-  return DEBUG_MODE;
+  initialiseSequelize(debugMode);
+  return debugMode;
 }
