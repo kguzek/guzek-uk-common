2ab998cfa9d55162db89a3719b4209bd89947df8
Konrad (PC) <konrad@guzek.uk>
2025-01-22 23:26:36 +0100

    implement method override logging

--- src/middleware/method-override.ts
+++ src/middleware/method-override.ts
@@ -1,8 +1,21 @@
 import methodOverride from "method-override";
+import { getLogger } from "../lib/logger";
+
+const logger = getLogger(__filename);
 
 export const useMethodOverride = methodOverride(
-  (req) =>
-    req.query._method ||
-    req.body._method ||
-    req.headers["x-http-method-override"]
+  (req) => {
+    const method =
+      req.query._method ||
+      req.body._method ||
+      req.headers["x-http-method-override"];
+    if (!method) return "POST";
+    if (typeof method !== "string") {
+      logger.warn("Invalid HTTP method override", method);
+      return "POST";
+    }
+    logger.http(`Request method override is ${method}`);
+    return method;
+  },
+  { methods: ["POST"] }
 );
